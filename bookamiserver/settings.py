1151ae61fb492a4464a8083d69e6e7b5490aca75
Milad Barazandeh <miladbeznd@gmail.com>
2026-04-23 03:03:05 +0900

    Refactor DEBUG and ALLOWED_HOSTS settings for improved clarity

--- bookamiserver/settings.py
+++ bookamiserver/settings.py
@@ -23,10 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = os.getenv("DEBUG", True) == "True"
 
 
-ALLOWED_HOSTS = ["*"] if DEBUG else []
+ALLOWED_HOSTS = (
+    ["*"]
+    if DEBUG
+    else [
+        "bookami.net",
+        "www.bookami.net",
+    ]
+)
 
 
 # Application definition
